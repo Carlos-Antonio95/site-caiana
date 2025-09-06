16eb5ee0dd1a0f2a1460071f776db28db6dd546d
Carlos Antonio <carlosantonioob@gmail.com>
2025-09-06 19:50:48 -0300

    Criado classes: addresses(model,controller,migration)

--- database/migrations/2025_09_06_213033_create_addresses_table.php
+++ database/migrations/2025_09_06_213033_create_addresses_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('id_clients');
+            $table->foreign('id_clients')->references('id')->on ('clients')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('addresses_name',200);
+            $table->string('road',200);//rua
+            $table->string('number',10);
+            $table->string('complement',50)->nullable()->default('Não informado');
+            $table->string('referenc',200)->nullable()->default('Não informado');
+            $table->string('neighborhood',100);//bairro kkk
+            $table->string('city',100);
+            $table->string('state',100);
+            $table->string('cep',20);
+            $table->string('country');//pais
             $table->timestamps();
         });
     }
