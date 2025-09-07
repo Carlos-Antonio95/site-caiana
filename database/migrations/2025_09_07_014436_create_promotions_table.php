123d0fb7278a8520c79e16682c8b437f931384a5
Carlos Antonio <carlosantonioob@gmail.com>
2025-09-06 23:35:27 -0300

    Criação de mais classes iniciais

---
+++ database/migrations/2025_09_07_014436_create_promotions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',150);
+            $table->text('descrition');
+            $table->date('start_date');
+            $table->date('find_Date');
+            $table->string('banner',255)->nullable();//provavelmente url da imagem ou arquivo talvez
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promotions');
+    }
+};
