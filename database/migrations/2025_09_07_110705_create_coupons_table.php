eed2b8385b4eb3f4d27779476d703cac13cab45b
Carlos Antonio <carlosantonioob@gmail.com>
2025-09-07 09:16:20 -0300

    contnuando criando o bacno de dados e model

---
+++ database/migrations/2025_09_07_110705_create_coupons_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Illuminate\Support\enum_value;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('code',50)->unique();
+            $table->enum('discount_type',['valor','percentual']);
+            $table->decimal('discount_value',10,2);
+            $table->decimal('min_discount',10,2);
+            $table->date('expiration_date');
+            $table->int('max_use');
+            $table->boolean('active');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coupons');
+    }
+};
