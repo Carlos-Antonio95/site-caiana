5ce9a7ef55500403fc3a209a03293d638c7a0ace
JoaoVitor <vitorsantanalins@gmail.com>
2025-09-09 01:53:35 -0300

    feat: Adiciona tabelas Carts, cart_items, paymants e adminactivities, além dos controllers Carts, CartItem, AdminActivity e Paymants, e dos model  Carts, CartItem, AdminActivity e Paymants

---
+++ database/migrations/2025_09_09_042846_create_carts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_clients')->nullable();
+            $table->foreign('id_clients')->references('id')->on('clients')->onUpdate('cascade')->onDelete('set null');
+            $table->string('token_session', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carts');
+    }
+};
